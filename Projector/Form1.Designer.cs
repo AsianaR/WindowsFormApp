bbe94fe7a1cd0b6a1b8f48f76c2c9b7fb374d61c
AsianaR <46110409+AsianaR@users.noreply.github.com>
2020-04-02 01:51:31 +0300

    Add files via upload

---
+++ Projector/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Projector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(452, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label1.Location = new System.Drawing.Point(72, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Money";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label2.Location = new System.Drawing.Point(325, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Car";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label3.Location = new System.Drawing.Point(542, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Bomb";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(326, 291);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(268, 245);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(152, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(287, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Натисніть готово коли картинки будуть на своїч місцях";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(217, 100);
+            this.panel1.TabIndex = 13;
+            this.panel1.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(248, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(216, 100);
+            this.panel2.TabIndex = 14;
+            this.panel2.Visible = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(483, 50);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(186, 100);
+            this.panel3.TabIndex = 14;
+            this.panel3.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.ImageLocation = "";
+            this.pictureBox2.Location = new System.Drawing.Point(309, 156);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(72, 67);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseUp);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.ImageLocation = "";
+            this.pictureBox3.Location = new System.Drawing.Point(540, 156);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(72, 67);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox3_MouseDown);
+            this.pictureBox3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox3_MouseMove);
+            this.pictureBox3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox3_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(77, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.PictureBox1_DragDrop);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 368);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Projector/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projector
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        string[,] a =   {{ "shrek.wmf", "death.wmf", "car.wmf" },
+                        { "tiger.wmf", "wolf.wmf", "circus.wmf" },
+                        { "shit.wmf", "starcraft.wmf", "c#.wmf" },
+                        { "DarkSouls.wmf", "you.wmf", "Died.wmf" } };
+        string[,] b = {{ "car", "death", "shrek" },
+                        { "wolf", "circus", "tiger" },
+                         { "starcraft","SHIT","C#" },
+                        { "You", "died", "DarkSouls" }};
+        int check1, check2, check3;
+
+        int[,] check = {
+        //  {1,2,3},
+          {3,2,1},
+          {3,1,2},
+          {2,1,3},
+          {3,1,2}
+};
+        int ShiftX, ShiftY;
+        public int i=0, score=0;
+        int left1, left2, left3;
+        int top1, top2, top3;
+        bool IsDown1,IsDown2,IsDown3;
+        int k = 4;
+        
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            score = (score * 100) / k;
+
+            MessageBox.Show((score) + " балів");
+
+        
+            Close();
+        }
+
+        private void PictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsDown1 = true;
+        }
+
+        private void PictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsDown2 = true;
+        }
+
+        private void PictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control c2 = sender as Control;
+            if (IsDown2)
+            {
+
+                c2.Location = this.PointToClient(Control.MousePosition);
+            }
+        }
+
+        private void PictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsDown2 = false;
+        }
+
+        private void PictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsDown3 = true;
+        }
+
+        private void PictureBox3_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control c3 = sender as Control;
+            if (IsDown3)
+            {
+
+                c3.Location = this.PointToClient(Control.MousePosition);
+            }
+        }
+
+        private void PictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsDown3 = false;
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control c = sender as Control;
+            if (IsDown1)
+            {
+
+                c.Location = this.PointToClient(Control.MousePosition);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            
+            if (IsItIN(pictureBox1, panel1))
+                check1 = 1;
+            if (IsItIN(pictureBox1, panel2))
+                check1 = 2;
+            if (IsItIN(pictureBox1, panel3))
+                check1 = 3;
+
+            if (IsItIN(pictureBox2, panel1))
+                check2 = 1;
+            if (IsItIN(pictureBox2, panel2))
+                check2 = 2;
+            if (IsItIN(pictureBox2, panel3))
+                check2 = 3;
+
+            if (IsItIN(pictureBox3, panel1))
+                check3 = 1;
+            if (IsItIN(pictureBox3, panel2))
+                check3 = 2;
+            if (IsItIN(pictureBox3, panel3))
+                check3 = 3;
+
+            if (check[i, 0] == check1 && check[i, 1] == check2 && check[i, 2] == check3)
+            {
+                MessageBox.Show("OK");
+                score += 1;
+                // SetPicture();
+            }
+            else
+            {
+                MessageBox.Show("Not OK");
+            }
+
+            if (i < 3)
+                i++;
+            else
+            {
+                score = (score * 100) / k;
+                MessageBox.Show((score) + " балів");
+
+                Close();
+            }
+
+
+            label1.Text = b[i, 0];
+                label2.Text = b[i, 1];
+                label3.Text = b[i, 2];
+
+                string link1 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/"+a[i, 0];
+                pictureBox1.Image = Image.FromFile(link1);
+                string link2 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/" + a[i, 1];
+                pictureBox2.Image = Image.FromFile(link2);
+                string link3 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/" + a[i, 2];
+                pictureBox3.Image = Image.FromFile(link3);
+
+            progressBar1.Value = 100;
+
+                pictureBox1.Left = left1;
+                pictureBox2.Left = left2;
+                pictureBox3.Left = left3;
+
+                pictureBox1.Top = top1;
+                pictureBox2.Top = top2;
+                pictureBox3.Top = top3;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            button1.Enabled = false;
+
+            
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsDown1 = false;
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            int count = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+            timer2.Interval = count;
+
+            if (IsItIN(pictureBox1, panel1) ^ IsItIN(pictureBox2, panel1) ^ IsItIN(pictureBox3, panel1) && IsItIN(pictureBox1, panel2) ^ IsItIN(pictureBox2, panel2) ^ IsItIN(pictureBox3, panel2) && IsItIN(pictureBox1, panel3) ^ IsItIN(pictureBox2, panel3) ^ IsItIN(pictureBox3, panel3))
+            {
+                button1.Enabled = true;
+
+            }
+            else
+                button1.Enabled = false;
+            if (progressBar1.Value == 0)
+            {
+                MessageBox.Show("Loser");
+                //SetPicture();
+
+                label1.Text = b[i, 0];
+                label2.Text = b[i, 1];
+                label3.Text = b[i, 2];
+
+                MessageBox.Show(a[i,1]);
+                string link1 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/" + a[i, 0];
+                pictureBox1.Image = Image.FromFile(link1);
+                string link2 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/" + a[i, 1];
+                pictureBox2.Image = Image.FromFile(link2);
+                string link3 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/" + a[i, 2];
+                pictureBox3.Image = Image.FromFile(link3);
+
+                progressBar1.Value = 100;
+
+                pictureBox1.Left = left1;
+                pictureBox2.Left = left2;
+                pictureBox3.Left = left3;
+
+                pictureBox1.Top = top1;
+                pictureBox2.Top = top2;
+                pictureBox3.Top = top3;
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+                button1.Enabled = false;
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            button1.Enabled = false;
+            progressBar1.Value = 100;
+
+
+            left1 = pictureBox1.Left;
+            left2 = pictureBox2.Left;
+            left3 = pictureBox3.Left;
+            top1  = pictureBox1.Top;
+            top2  = pictureBox2.Top;
+            top3  = pictureBox3.Top;
+            //SetPicture();
+
+            label1.Text = b[i, 0];
+            label2.Text = b[i, 1];
+            label3.Text = b[i, 2];
+
+            string link1 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/" + a[i, 0];
+            pictureBox1.Image = Image.FromFile(link1);
+            string link2 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/" + a[i, 1];
+            pictureBox2.Image = Image.FromFile(link2);
+            string link3 = "C:/Users/user/source/repos/Projector/bin/Debug/Images/" + a[i, 2];
+            pictureBox3.Image = Image.FromFile(link3);
+
+            progressBar1.Value = 100;
+
+            pictureBox1.Left = left1;
+            pictureBox2.Left = left2;
+            pictureBox3.Left = left3;
+
+            pictureBox1.Top = top1;
+            pictureBox2.Top = top2;
+            pictureBox3.Top = top3;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            button1.Enabled = false;
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (label4.Visible == true)
+                label4.Visible = false;
+            else
+                label4.Visible = true;
+
+            
+
+            if (progressBar1.Value >= 1)
+                progressBar1.Value -= 1;
+            else
+                MessageBox.Show("Time over");
+        }
+
+
+       public static bool IsItIN(PictureBox TImage, Panel TShape)
+        {
+            if (TImage.Left >= TShape.Left && TImage.Left + TImage.Width <= TShape.Left + TShape.Width && TImage.Top >= TShape.Top && TImage.Top + TImage.Height <= TShape.Top + TShape.Height)
+            {
+                return true;
+               }
+            else return false;
+        }
+            
+    }
+}
